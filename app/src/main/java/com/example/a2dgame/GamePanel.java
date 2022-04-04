d683da35096b6b1f3cba3b2a60f747aef997f51c
deepak-a-07 <deepakanand093@gmail.com>
2022-04-04 20:03:22 +0530

    second commit

--- app/src/main/java/com/example/a2dgame/GamePanel.java
+++ app/src/main/java/com/example/a2dgame/GamePanel.java
@@ -3,6 +3,7 @@ package com.example.a2dgame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.MotionEvent;
@@ -16,9 +17,7 @@ import android.view.SurfaceView;
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
 
-    private RectPlayer player;
-    private Point playerPoint;
-    private ObstacleManager obstacleManager;
+    private SceneManager manager;
 
     public GamePanel(Context context) {
         super(context);
@@ -27,10 +26,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
         thread = new MainThread(getHolder(), this);
 
-        player = new RectPlayer(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
-        playerPoint = new Point(150,150);
-
-        obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
+        manager = new SceneManager();
 
         setFocusable(true);
     }
@@ -62,28 +58,21 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public boolean onTouchEvent (MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-            case MotionEvent.ACTION_MOVE:
-                playerPoint.set((int)event.getX(), (int)event.getY());
-        }
+
+        manager.receiveTouch(event);
 
         return true;
         // return super.onTouchEvent(event);
     }
 
     public void update() {
-        player.update(playerPoint);
-        obstacleManager.update();
+        manager.update();
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        canvas.drawColor(Color.WHITE);
-
-        player.draw(canvas);
-        obstacleManager.draw(canvas);
+        manager.draw(canvas);
     }
 }
