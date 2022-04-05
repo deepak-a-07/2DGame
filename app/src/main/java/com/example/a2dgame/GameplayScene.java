64ab876c9f3453b463e82b180f84636bf06e0d16
deepak-a-07 <deepakanand093@gmail.com>
2022-04-05 22:20:29 +0530

    fourth commit

--- app/src/main/java/com/example/a2dgame/GameplayScene.java
+++ app/src/main/java/com/example/a2dgame/GameplayScene.java
@@ -99,8 +99,8 @@ public class GameplayScene implements Scene {
                 float pitch = orientationData.getOrientation()[1] - orientationData.getStartOrientation()[1];
                 float roll = orientationData.getOrientation()[2] - orientationData.getStartOrientation()[2];
 
-                float xSpeed = 2 * roll * Constants.SCREEN_WIDTH/1000f;
-                float ySpeed = pitch * Constants.SCREEN_HEIGHT/1000f;
+                float xSpeed = 2 * roll * Constants.SCREEN_WIDTH/1500f;
+                float ySpeed = pitch * Constants.SCREEN_HEIGHT/1500f;
 
                 playerPoint.x += Math.abs(xSpeed*elapsedTime) > 5 ? xSpeed*elapsedTime : 0;
                 playerPoint.y -= Math.abs(ySpeed*elapsedTime) > 5 ? ySpeed*elapsedTime : 0;
--- app/src/main/java/com/example/a2dgame/MainActivity.java
+++ app/src/main/java/com/example/a2dgame/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.a2dgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
-
+    MediaPlayer mySong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +22,8 @@ public class MainActivity extends Activity {
         Constants.SCREEN_HEIGHT = dm.heightPixels;
 
         setContentView(new GamePanel(this));
+        mySong = MediaPlayer.create(MainActivity.this, R.raw.music);
+        mySong.start();
     }
+
 }
